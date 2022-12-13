e88ff9d0cf3e229f02ff773b0c7c0d0e03c35787
Ming <102708759+MingK4@users.noreply.github.com>
2022-12-13 14:51:47 +0700

    Initial commit

---
+++ UserInfos/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String name;
+        String address;
+        int age;
+
+        Scanner inputs = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String uName = inputs.nextLine();
+        System.out.println("Please enter your address: ");
+        String uAddress = inputs.nextLine();
+        System.out.println("Please enter your age: ");
+        int uAge = inputs.nextInt();
+        
+    }
+}
