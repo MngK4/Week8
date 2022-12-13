f369b2a090cc1330d54ad6bd9dee43e1c7c269fe
Ming <102708759+MingK4@users.noreply.github.com>
2022-12-13 15:36:47 +0700

    Finished the first excercise

--- UserInfos/src/Main.java
+++ UserInfos/src/Main.java
@@ -1,18 +1,37 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        String name;
-        String address;
+    public static void main(String[] args) throws IOException {
+        String name, fileName, address;
         int age;
+        PrintWriter pw;
 
         Scanner inputs = new Scanner(System.in);
-        System.out.println("Please enter your name: ");
-        String uName = inputs.nextLine();
-        System.out.println("Please enter your address: ");
-        String uAddress = inputs.nextLine();
-        System.out.println("Please enter your age: ");
-        int uAge = inputs.nextInt();
-        
+
+        System.out.println("Enter the name of the file: ");
+        fileName = inputs.nextLine();
+
+        if (Files.notExists(Path.of(fileName))) {
+            pw = new PrintWriter(new FileWriter(fileName));
+        } else {
+            pw = new PrintWriter(new FileWriter(fileName, true));
+        }
+        do {
+            System.out.println("Please enter your name: ");
+            name = inputs.nextLine();
+            System.out.println("Please enter your address: ");
+            address = inputs.nextLine();
+            System.out.println("Please enter your age: ");
+            age = inputs.nextInt();
+            inputs.nextLine();
+            System.out.println("Do you want to continue? ");
+            pw.println(name + ", " + address + ", " + age);
+        } while (inputs.nextLine().charAt(0) == 'Y');
+        pw.close();
     }
 }
